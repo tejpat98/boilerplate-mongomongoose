9cf9b862649a5578f62599f4cfe1662bc838eac8
Tejash Patel <tejpat98@gmail.com>
2023-02-15 12:39:28 +0000

    Update myApp.js
    12/12

--- myApp.js
+++ myApp.js
@@ -95,8 +95,10 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({ name: 'asc' }).limit(2).select('-age').exec(function(err, data){
+    if(err){return console.log(err)}
+    done(null, data)
+  })
 };
 
 /** **Well Done !!**
